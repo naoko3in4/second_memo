aee624adbea575082749b924f5ae9a5dae2a845b
Naoko3in4 <okashimochi@gimail.com>
2021-01-10 23:51:34 +0900

    create showMemo function and form

--- backend/app/Http/Controllers/MemoController.php
+++ backend/app/Http/Controllers/MemoController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Memo;
+
 class MemoController extends Controller
 {
   public function showMemo()
   {
     // メモを表示
-    return view('memo');
-    //dd('showMemoだよ');
+
+    // memo.blade.php のフォームの入力値を取得
+    // memo.blade.phpに表示
+    $memos = Memo::get();
+    //dd($memos);
+    return view('memo', ['memos' => $memos]);
+
+    // dbへ値を追加
+    //$memo = Memo::create([]);
   }
 }
--- backend/resources/views/memo.blade.php
+++ backend/resources/views/memo.blade.php
@@ -3,11 +3,27 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>home画面</title>
+  <title>メモ</title>
 </head>
 
 <body>
-  <h1>ホーム画面です。</h1>
+  <h1>メモ入力画面です。</h1>
+
+  <tbody>
+    @foreach ($memos as $memo)
+    <tr>
+      <td>タイトル：{{ $memo->title }}</td>
+      <td>内容：{{ $memo->content }}</td>
+    </tr>
+    @endforeach
+  </tbody>
+
+  <form action="memo" method="post">
+    <input type="text" name="title" placeholder="タイトル">
+    <input type="text" name="content" placeholder="メモ内容">
+    <input type="submit" value="送信">
+    <input type="hidden" name="_token" value="{{csrf_token()}}">
+  </form>
 </body>
 
 </html>
