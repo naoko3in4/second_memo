dc4f034ed39d130a6f798e8b091f3f5cd70fab22
Naoko3in4 <okashimochi@gimail.com>
2021-01-10 17:31:01 +0900

    create MemoController

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MemoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    logger('welcome route.');
     return view('welcome');
 });
+
+Route::get('/memo', [
+    MemoController::class, 'showMemo'
+]);
