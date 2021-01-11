069da674a43144da9fddcc5936fe778c89c86b04
Naoko3in4 <okashimochi@gimail.com>
2021-01-11 17:20:58 +0900

    route setting and create store function

--- backend/app/Http/Controllers/MemoController.php
+++ backend/app/Http/Controllers/MemoController.php
@@ -3,20 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Memo;
+use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
   public function showMemo()
   {
     // メモを表示
-
     // memo.blade.php のフォームの入力値を取得
     // memo.blade.phpに表示
     $memos = Memo::get();
-    //dd($memos);
     return view('memo', ['memos' => $memos]);
+  }
 
+  public function store(Request $request)
+  {
     // dbへ値を追加
-    //$memo = Memo::create([]);
+    $memo_title = $request->input('title');
+    $memo_content = $request->input('content');
+    Memo::create(['title' => $memo_title, 'content' => $memo_content]);
+    return redirect('/memo');
+    // return redirect()->back()->with('success_message', 'SUCCESS!!');
   }
 }
